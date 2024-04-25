781e98aa73878a7d11c295e65d770ce306b7c4d2
sagar budhathoki <sagar@eventlogic.se>
2024-04-25 12:00:50 +0545

    Sample app for Knowledge Sharing Docker Session - Blogging App

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import requests
 
 app = Flask(__name__)
 
-# Function to fetch URL preview data
 def get_url_preview(url):
     try:
         response = requests.get(url)
@@ -20,7 +19,6 @@ def get_url_preview(url):
         print(f"Error fetching URL preview for {url}: {e}")
         return None
 
-# Function to embed URL previews into Markdown content
 def embed_url_preview(markdown_content):
     try:
         soup = BeautifulSoup(markdown_content, 'html.parser')
@@ -39,14 +37,12 @@ def embed_url_preview(markdown_content):
 
 @app.route('/')
 def index():
-    # Read the Markdown file and embed URL previews
     markdown_file = "docker_session_1.md"
     with open(markdown_file, 'r') as file:
         markdown_content = file.read()
     
     html_content = embed_url_preview(markdown_content)
     
-    # Convert Markdown to HTML
     html_content = markdown2.markdown(html_content, extras=["tables", "code-friendly", "fenced-code-blocks", "metadata", "latex", "footnotes", "smarty-pants", "strike", "tag-friendly", "xml"])
 
     # soup = BeautifulSoup(html_content, 'html.parser')
@@ -71,41 +67,12 @@ def preview():
 
 @app.route('/posts')
 def blogs():
-    html_content = """
-        - Example:
-    - **Run MySQL Container**: Start the MySQL container with a name `mysql-container`, exposing port 3306:
-        
-        ```bash
-        docker run --name mysql-container -e MYSQL_ROOT_PASSWORD=password -d mysql:latest
-        ```
-        
-    - **Create a .NET Core Application**: Assume you have a .NET Core application that needs to connect to the MySQL database. Build the .NET Core application and create a Docker image for it. Here's a simple Dockerfile assuming the application is published to a folder named `app`:
-        
-        ```
-        FROM mcr.microsoft.com/dotnet/core/runtime:latest
-        WORKDIR /app 
-        COPY ./app .
-        ENTRYPOINT ["dotnet", "YourApp.dll"]
-        ```
-        
-    - **Run .NET Core Application Container Linked to MySQL**: Now, run the .NET Core application container, linking it to the MySQL container:
-        
-        ```bash
-        docker run --name dotnet-app --link mysql-container:mysql -d your-dotnet-image:latest
-        ```
-        
-    
-    In this example:
-    
-    - `-name mysql-container` assigns the name `mysql-container` to the MySQL container.
-    - `e MYSQL_ROOT_PASSWORD=password` sets the root password for MySQL.
-    - `-name dotnet-app` assigns the name `dotnet-app` to the .NET Core application container.
-    - `-link mysql-container:mysql` links the .NET Core application container to the MySQL container with an alias `mysql`.
-    - `d` runs both containers in detached mode.
-    
-    Inside the .NET Core application container, you can now access the MySQL database using the hostname `mysql` and the exposed port. Make sure your .NET Core application is configured to connect to MySQL using the correct hostname and port.
-    
+    md_content = """
+        - just test
+            - testing child content
+            - testing **bold** content
     """
+    html_content = markdown2.markdown(md_content,["tables", "fenced-code-blocks"])
     return render_template('index.html', content=html_content)
 
 if __name__ == '__main__':
